0c2b8c49d4330b76d49729c425f843d8712bcf77
Kristoffer Ahlqvist <ahlqvistk@debian-ux.ap.local>
2018-05-17 22:02:26 +0200

    Add maps component, change nav, subscribe redraw

--- src/components/start.js
+++ src/components/start.js
@@ -3,10 +3,10 @@ import m from 'mithril';
 export default {
     view: () => (
         <nav>
-            <button>Maps</button>
-            <button>Flags</button>
-            <button>Capitals</button>
-            <button>Full game</button>
+            <a href='/maps' oncreate={m.route.link}>Maps</a>
+            <a href='/flags' oncreate={m.route.link}>Flags</a>
+            <a href='/capitals' oncreate={m.route.link}>Capitals</a>
+            <a href='/full' oncreate={m.route.link}>Full game</a>
         </nav>
     ),
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /* global document */
 import m from 'mithril';
+
 import getCountries from './data/get-countries';
+import store from './store';
+
+import Maps from './components/maps';
 import Start from './components/start';
 
 getCountries();
 
 m.route(document.body, '/', {
     '/': Start,
+    '/maps': Maps,
 });
+
+store.subscribe(m.redraw);
