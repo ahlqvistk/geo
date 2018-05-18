a1e2d6401a52556856c6bf282bad15004a9feb46
Kristoffer Ahlqvist <ahlqvistk@debian-ux.ap.local>
2018-05-18 09:26:07 +0200

    Add questions and alternatives actions-reducers

--- src/actions.js
+++ src/actions.js
@@ -11,3 +11,13 @@ export const fetchingCountriesFailed = (err) => ({
     type: 'FETCHING_COUNTRIES_FAILED',
     payload: err,
 });
+
+export const countryQuestionsSelected = (questions) => ({
+    type: 'COUNTRY_QUESTIONS_SELECTED',
+    payload: questions,
+});
+
+export const countryAlternativesSelected = (alternatives) => ({
+    type: 'COUNTRY_ALTERNATIVES_SELECTED',
+    payload: alternatives,
+});
--- src/reducers.js
+++ src/reducers.js
@@ -3,6 +3,8 @@ import {Countries} from './types';
 
 export default combineReducers({
     countries,
+    countryAlternatives,
+    countryQuestions,
 });
 
 function countries(state = Countries.Unfetched, action) {
@@ -13,6 +15,24 @@ function countries(state = Countries.Unfetched, action) {
         return Countries.Fetched(action.payload);
     case 'FETCHING_COUNTRIES_FAILED':
         return Countries.Failed(action.payload);
+    default:
+        return state;
+    }
+}
+
+function countryAlternatives(state = [], action) {
+    switch (action.type) {
+    case 'COUNTRY_ALTERNATIVES_SELECTED':
+        return action.payload;
+    default:
+        return state;
+    }
+}
+
+function countryQuestions(state = [], action) {
+    switch (action.type) {
+    case 'COUNTRY_QUESTIONS_SELECTED':
+        return action.payload;
     default:
         return state;
     }
